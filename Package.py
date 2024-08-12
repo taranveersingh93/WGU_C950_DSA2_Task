30e783dd65959a6afeb472d42ac0475132397e86
Taranveer Singh <taranveersingh93@gmail.com>
2024-08-12 16:11:38 -0400

    Add methods to read packages and addresses

--- Package.py
+++ Package.py
@@ -1,5 +1,5 @@
 class Package:
-    def __init__(self, id, address, deadline, city, zipCode, weight, deliveryStatus, truckAffinity):
+    def __init__(self, id, address, deadline, city, zipCode, weight, deliveryStatus, note, truckAffinity):
         self.id = id
         self.address = address
         self.deadline = deadline
@@ -7,5 +7,6 @@ class Package:
         self.zipCode = zipCode
         self.weight = weight
         self.deliveryStatus = deliveryStatus
+        self.note = note
         self.truckAffinity = truckAffinity
 
