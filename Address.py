e6e50870bf0da34fb7c468b572a828b46a2c6fcf
Taranveer Singh <taranveersingh93@gmail.com>
2024-08-16 14:15:25 -0400

    Add comments for address class

--- Address.py
+++ Address.py
@@ -1,16 +1,21 @@
 class Address:
+    # Constructor
     def __init__(self, location, address):
         self.location = location
         self.address = address
 
+    # getter for location
     def get_location(self):
         return self.location
 
+    # getter for address
     def get_address(self):
         return self.address
 
+    # setter for location
     def set_location(self, new_location):
         self.location = new_location
 
+    # setter for address
     def set_address(self, new_address):
         self.address = new_address
